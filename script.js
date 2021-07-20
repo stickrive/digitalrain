979fc966239818822ff7d39dbb3783631fa4fb53
PaulB-H <p.bernardhall@gmail.com>
2021-07-19 23:09:15 -0400

    script: var->let/const, draw changed to arrow fn

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ context.translate(canvas.width, 0);
 context.scale(-1, 1);
 
 //Half-width kana characters, Latin Numbers and Alphabets, in decreasing probability.
-var chars =
+const chars =
   "｢｣ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ｢｣ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ｢｣ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ123456789123456789123456789123456789123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
     ""
   );
 
-for (var i = 0; i < columns; i++) drops[i] = Math.random() * 50 - 50;
+for (let i = 0; i < columns; i++) drops[i] = Math.random() * 50 - 50;
 
-function draw() {
+const draw = () => {
   // Black background with 0.25 opacity to show the trail.
   context.font = font_size + "px 'Consolas', 'Lucida Console'";
   context.fillStyle = "rgba(0, 0, 0, 0.025)";
@@ -27,13 +27,13 @@ function draw() {
 
   //Set the previous line to green so that the trail would remain green.
   context.fillStyle = "#6D2";
-  for (var i = 0; i < drops.length; i++) {
+  for (let i = 0; i < drops.length; i++) {
     context.fillText(text[i], i * font_size, drops[i] * font_size);
   }
 
   // Generate new characters and display them, in white.
   context.fillStyle = "#FFF";
-  for (var i = 0; i < drops.length; i++) {
+  for (let i = 0; i < drops.length; i++) {
     drops[i]++;
     // Random character to print.
     text[i] = chars[Math.floor(Math.random() * chars.length)];
@@ -44,6 +44,6 @@ function draw() {
     if (drops[i] * font_size > canvas.height)
       drops[i] = Math.random() * 100 - 100;
   }
-}
+};
 
 setInterval(draw, 75);
